03fee76aa1f8daf620d0be53ab857abb5d600535
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-01-25 01:12:49 +0530

    Fixed Test issue for ViewModel.

--- app/src/main/java/com/example/androidmvvmwithcleandemo/post/common/utils/Validator.kt
+++ app/src/main/java/com/example/androidmvvmwithcleandemo/post/common/utils/Validator.kt
@@ -1,12 +1,18 @@
 package com.example.androidmvvmwithcleandemo.post.common.utils
 
+
+import android.text.TextUtils
 import androidx.core.util.PatternsCompat
 
 
 class Validator {
     companion object
     {
-        fun isUserNameValid(username: String): Boolean =  PatternsCompat.EMAIL_ADDRESS.matcher(username).matches();
+        fun isUserNameValid(username: String): Boolean =  !isEmpty(username) && PatternsCompat.EMAIL_ADDRESS.matcher(username).matches();
         fun isPasswordValid(password: String): Boolean = password.length > 3
+
+        fun isEmpty(str: CharSequence?): Boolean {
+            return str == null || str.length == 0
+        }
     }
 }
